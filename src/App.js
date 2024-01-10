236e92d351bed748edc9816ffebf545a15e72bd9
tanay shah <tanayshah027@gmail.com>
2024-01-11 00:32:19 +0530

    dummy change to build. Given write access to workflows

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { useTheme } from './Contexts/ThemeContext';
 
 function App() {
   const { theme } = useTheme();
+
   return (
     <div id="mainElement" className={theme === 'light' ? 'light-mode' : 'dark-mode' }>
     <HashRouter>
