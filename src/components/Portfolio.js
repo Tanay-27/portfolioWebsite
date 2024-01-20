f279626d23989afcf8bd210ff90e11505cfdf6e1
tanay shah <tanayshah027@gmail.com>
2024-01-20 17:24:04 +0530

    hardcoded in case api doesnt work

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Portfolio.scss';
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -33,13 +33,15 @@ const PinnedRepos = ({ username }) => {
         );
 
         if (!response.ok) {
-          throw new Error(`Error fetching pinned repositories: ${response.statusText}`);
+          setPinnedRepos(repoData)
+        }
+        else{
+          const data = await response.json();
+          setPinnedRepos(data) ; //.filter(repo => toShowProjectsName.includes(repo.name)));
         }
         // const toShowProjectsName = ['ML_Hackathons-Projects','HandWritten_OCR','Video_Chat_App','Machine_Vision_Projects','ChatApp','75_Days_Hard'];
-        const data = await response.json();
-        setPinnedRepos(data) ; //.filter(repo => toShowProjectsName.includes(repo.name)));
       } catch (error) {
-        console.error(error.message);
+        setPinnedRepos(repoData);
       }
     };
 
@@ -58,3 +60,53 @@ const PinnedRepos = ({ username }) => {
     </div>
   );
 };
+
+
+const repoData = [
+  {
+    "name": "portfolioWebsite",
+    "description": "Created Portfolio website using Reactjs, HTML and CSS",
+    "url": "https://api.github.com/repos/Tanay-27/portfolioWebsite",
+  },
+  {
+    "name": "Video_Chat_App",
+    "description": "A group video calling application that allows all users with the link to video call in real time. Uses WebRTC and socket communication",
+    "url": "https://api.github.com/repos/Tanay-27/Video_Chat_App",
+  },
+  {
+    "name": "Machine_Vision_Projects",
+    "description": "Machine Vision projects including but not limited to Face Recognition, Gaze Detection, Handwriting Recognition, Smile Detection, Drowsiness Detection, etc",
+    "url": "https://api.github.com/repos/Tanay-27/Machine_Vision_Projects",
+  },
+  {
+    "name": "HandWritten_OCR",
+    "description": "Handwritten Character Recognition using NIST dataset",
+    "url": "https://api.github.com/repos/Tanay-27/HandWritten_OCR",
+  },
+  {
+    "name": "ChatApp",
+    "description": "Node.js and MongoDB real-time chat application",
+    "url": "https://api.github.com/repos/Tanay-27/ChatApp",
+  },
+  {
+    "name": "75_Days_Hard",
+    "description": "Documenting my journey of taking up 75 days non-stop incremental practise/learning of concepts",
+    "url": "https://api.github.com/repos/Tanay-27/75_Days_Hard",
+  },
+  {
+    "name": "ML_Hackathons-Projects",
+    "description": "A collection of ML models created for different Hackathons and projects. Jupyter notebooks ready to use in Jupyter or VSCode",
+    "url": "https://api.github.com/repos/Tanay-27/ML_Hackathons-Projects",
+  },
+  {
+    "name": "paymentapi",
+    "description": "Integration of Instamojo Payment API Sandbox into Python Code",
+    "url": "https://api.github.com/repos/Tanay-27/paymentapi",
+  },
+  {
+    "name": "send-emails",
+    "description": "Python Script to send Emails to multiple recipients",
+    "url": "https://api.github.com/repos/Tanay-27/send-emails",
+  },
+]
+
